e11591d455bed063be4cefd67fc2283612e4a8cd
felix1251 <felixabacajen17@gmail.com>
2024-08-05 15:13:00 +0800

    remove public assets - must be ignores

--- erpweb/app/controllers/customer_portal/dashboard_controller.rb
+++ erpweb/app/controllers/customer_portal/dashboard_controller.rb
@@ -7,25 +7,24 @@ module CustomerPortal
     def index
       @orders = current_user.customer.orders if current_user.customer
       @support_tickets = current_user.customer.support_tickets if current_user.customer
-    #else
-      #Rails.logger.debug "Current user does not have an associated customer"
-      #@orders = []
-      #@support_tickets = []
+      # else
+      # Rails.logger.debug "Current user does not have an associated customer"
+      # @orders = []
+      # @support_tickets = []
     end
 
-  private
+    private
 
-  def ensure_customer_user
-    unless current_user.customer_user?
-      redirect_to root_path, alert: "You do not have access to the customer portal."
+    def ensure_customer_user
+      return if current_user.customer_user?
+
+      redirect_to root_path, alert: 'You do not have access to the customer portal.'
+    end
+
+    def generate_jws_token(user)
+      payload = { user_id: user.id }
+      secret = Rails.application.secrets.secret_key_base
+      JWT.encode(payload, secret, 'HS256')
     end
   end
-
-  def generate_jws_token(user)
-    payload = { user_id: user.id }
-    secret = Rails.application.secrets.secret_key_base
-    JWT.encode(payload, secret, 'HS256')
-  end
-
-end
 end
