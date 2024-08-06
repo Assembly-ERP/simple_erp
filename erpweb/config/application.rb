7c862be4906588de13417f1598a1f24fb059eb43
felix1251 <felixabacajen17@gmail.com>
2024-08-06 16:57:33 +0800

    add haml config

--- erpweb/config/application.rb
+++ erpweb/config/application.rb
@@ -24,6 +24,11 @@ module Erpweb
 
     config.middleware.delete Rack::Sendfile
 
+    config.generators do |g|
+      g.template_engine = :haml
+      g.test_framework(:rspec, fixtures: false, view_specs: false, helper_specs: false, routing_specs: false)
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
