402baee996dc9a55a6d3fa6070d60265d4c0650d
felix1251 <felixabacajen17@gmail.com>
2024-08-13 15:43:14 +0800

    update migrate support ticket table

--- db/migrate/20240515175039_create_support_tickets.rb
+++ db/migrate/20240515175039_create_support_tickets.rb
@@ -2,13 +2,12 @@
 
 class CreateSupportTickets < ActiveRecord::Migration[7.1]
   def change
-    # Create support_tickets table
     create_table :support_tickets do |t|
       t.references :user, null: false, foreign_key: true
-      t.text :issue_description
-      t.string :status
+      t.text :issue_description, null: false
+      t.string :status, null: false, default: 'pending'
       t.references :customer, foreign_key: true
-      t.string :title
+      t.string :title, null: false
 
       t.timestamps
     end
--- db/migrate/20240812231045_update_customer_col_name_uniq.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class UpdateCustomerColNameUniq < ActiveRecord::Migration[7.1]
-  def change
-    add_index :customers, :name, unique: true
-  end
-end
