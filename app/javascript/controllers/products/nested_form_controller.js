24b2e300c64318f366f885d9712008cf5cbf6440
felix1251 <felixabacajen17@gmail.com>
2024-08-15 19:10:33 +0800

    product nested form update

--- app/javascript/controllers/products/nested_form_controller.js
+++ app/javascript/controllers/products/nested_form_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["target", "template", "addedList"];
+  static targets = ["target", "template", "addedPart"];
 
   static values = {
     wrapperSelector: { type: String, default: ".nested-form-wrapper" },
@@ -9,7 +9,26 @@ export default class extends Controller {
 
   add(e) {
     e.preventDefault();
+
+    e.target.classList.remove("bg-[color:var(--primary)]");
+    e.target.classList.add("bg-[color:var(--secondary)]");
+    e.target.innerHTML = "Update";
+
     const dataset = e.target.dataset;
+    const checkedPart = this.checkPart(dataset.partId);
+
+    if (checkedPart) {
+      if (this.isNewPart) this.appendPart(dataset, checkedPart);
+      else {
+        /* update via turbo-stream*/
+      }
+      return;
+    }
+
+    this.appendPart(dataset);
+  }
+
+  appendPart(dataset, replaceEl = null) {
     const totalPrice = (
       Number(dataset.price) * Number(dataset.quantity)
     ).toFixed(2);
@@ -19,23 +38,37 @@ export default class extends Controller {
       .replace("{{name}}", dataset.name)
       .replace(/{{quantity}}/g, dataset.quantity)
       .replace("{{price}}", totalPrice)
-      .replace("{{part-id}}", dataset.partId);
+      .replace(/{{part-id}}/g, dataset.partId);
 
-    this.targetTarget.insertAdjacentHTML("beforebegin", template);
+    if (!replaceEl)
+      this.targetTarget.insertAdjacentHTML("beforebegin", template);
+    else replaceEl.outerHTML = template;
+
     const event = new CustomEvent("rails-nested-form:add", { bubbles: !0 });
     this.element.dispatchEvent(event);
   }
 
   remove(e) {
     e.preventDefault();
+
     const closest = e.target.closest(this.wrapperSelectorValue);
+
     if (closest.dataset.newRecord === "true") t.remove();
     else {
       closest.style.display = "none";
       const destroy = closest.querySelector("input[name*='_destroy']");
       destroy.value = "1";
     }
+
     const event = new CustomEvent("rails-nested-form:remove", { bubbles: !0 });
     this.element.dispatchEvent(event);
+  }
+
+  checkPart(id) {
+    return document.querySelector(`[id='part-form-${id}']`);
+  }
+
+  get isNewPart() {
+    return this.element.dataset.isNew == "true";
   }
 }
--- db/migrate/20240603222639_create_join_table_products_parts.rb
+++ db/migrate/20240603222639_create_join_table_products_parts.rb
@@ -3,8 +3,7 @@
 class CreateJoinTableProductsParts < ActiveRecord::Migration[7.1]
   def change
     create_join_table :products, :parts do |t|
-      t.index %i[product_id part_id]
-      t.index %i[part_id product_id]
+      t.index %i[part_id product_id], unique: true
       t.integer :quantity, default: 1
     end
   end
