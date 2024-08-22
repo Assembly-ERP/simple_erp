3a2cf7ecf327530cb83aa288ca206630f202a368
felix1251 <felixabacajen17@gmail.com>
2024-08-22 22:01:22 +0800

    orders controller cleanup

--- app/controllers/operational_portal/orders_controller.rb
+++ app/controllers/operational_portal/orders_controller.rb
@@ -20,7 +20,7 @@ module OperationalPortal
       if @order.save
         redirect_to operational_portal_order_path(@order), notice: 'Order created successfully.'
       else
-        render :new
+        render :new, status: :unprocessable_entity
       end
     end
 
@@ -37,42 +37,11 @@ module OperationalPortal
       redirect_to operational_portal_orders_path, notice: 'Order was successfully cancelled.'
     end
 
-    def search_items
-      query = params[:query]
-      parts = Part.where('name ILIKE ?', "%#{query}%").map do |p|
-        p.attributes.merge(type: 'part', in_stock: p.in_stock)
-      end
-      products = Product.where('name ILIKE ?', "%#{query}%").map do |p|
-        p.attributes.merge(type: 'product', in_stock: nil)
-      end
-
-      render json: (parts + products)
-    end
-
     private
 
     def order_params
       params.require(:order).permit(:status, :customer_id,
                                     order_details_attributes: %i[id product_id part_id quantity price _destroy])
-    end
-
-    def part_attributes(part)
-      {
-        id: part.id,
-        name: part.name,
-        description: part.description,
-        price: part.price,
-        inventory: part.in_stock
-      }
-    end
-
-    def product_attributes(product)
-      {
-        id: product.id,
-        name: product.name,
-        description: product.description,
-        price: product.price
-      }
     end
   end
 end
