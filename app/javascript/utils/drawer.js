5a9695f333338991f5fc6667c1e1f07fd2a3cf7d
felix1251 <felixabacajen17@gmail.com>
2024-08-26 21:19:21 +0800

    catalog union query and js utils

---
+++ app/javascript/utils/drawer.js
@@ -0,0 +1,20 @@
+export const openClassNames = {
+  right: "translate-x-0",
+  left: "translate-x-0",
+  top: "translate-y-0",
+  bottom: "translate-y-0",
+};
+
+export const closeClassNames = {
+  right: "translate-x-full",
+  left: "-translate-x-full",
+  top: "-translate-y-full",
+  bottom: "translate-y-full",
+};
+
+export const classNames = {
+  right: "inset-y-0 right-0",
+  left: "inset-y-0 left-0",
+  top: "inset-x-0 top-0",
+  bottom: "inset-x-0 bottom-0",
+};
