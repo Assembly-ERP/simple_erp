b935a65ce98aee89e2eec4bd3575ccb13bc17278
felix1251 <felixabacajen17@gmail.com>
2024-08-31 02:58:20 +0800

    dahsboard display only order that is not voided

--- app/controllers/operational_portal/dashboard_controller.rb
+++ app/controllers/operational_portal/dashboard_controller.rb
@@ -5,7 +5,7 @@ module OperationalPortal
     authorize_resource class: false
 
     def index
-      @orders = Order.order(created_at: :desc).limit(10).with_customer.with_order_status
+      @orders = Order.order(created_at: :desc).limit(10).with_customer.with_order_status.not_voided
       @support_tickets = SupportTicket.order(created_at: :desc).limit(10).with_customer
     end
   end
