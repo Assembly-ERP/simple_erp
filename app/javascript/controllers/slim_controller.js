ef7f1f2bcee9ed789c2f65913296a5eed04a6771
felix1251 <felixabacajen17@gmail.com>
2024-08-31 00:52:53 +0800

    add status selection in edit

---
+++ app/javascript/controllers/slim_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus";
+import SlimSelect from "slim-select";
+
+export default class extends Controller {
+  connect() {
+    new SlimSelect({ select: this.element });
+  }
+}
