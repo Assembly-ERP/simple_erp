8167168d1c3b486be5c9d712b2122f0fd0a514fd
felix1251 <felixabacajen17@gmail.com>
2024-09-05 22:16:20 +0800

    remove aws

--- config/initializers/aws.rb
+++
@@ -1,9 +0,0 @@
-require 'aws-sdk-core'
-
-Aws.config.update(
-  {
-    region: ENV.fetch('S3_REGION', nil),
-    endpoint: ENV.fetch('S3_ENDPOINT', nil),
-    credentials: ENV.fetch('S3_ACCESS_KEY_ID', nil)
-  }
-)
