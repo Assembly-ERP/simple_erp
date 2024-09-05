5f5a860affd6ed5a5a26ee380a82bca755d28661
felix1251 <felixabacajen17@gmail.com>
2024-09-05 23:04:28 +0800

    disconnect swiper

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -4,8 +4,6 @@ Aws.config.update(
   {
     region: ENV.fetch('S3_REGION', nil),
     endpoint: ENV.fetch('S3_ENDPOINT', nil),
-    credentials:
-    Aws::Credentials.new(ENV.fetch('S3_ACCESS_KEY_ID', nil),
-                         ENV.fetch('S3_ACCESS_KEY', nil))
+    credentials: Aws::Credentials.new(ENV.fetch('S3_ACCESS_KEY_ID', nil), ENV.fetch('S3_ACCESS_KEY', nil))
   }
 )
