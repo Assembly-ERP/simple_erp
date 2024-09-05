2259050e083428b343fa7b099641ac72290cd153
felix1251 <felixabacajen17@gmail.com>
2024-09-05 21:34:57 +0800

    setup s3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,8 @@
-# config/environments/development.rb
-
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.active_storage.service = :amazon
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
