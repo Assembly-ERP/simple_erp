eb54f4e885ee0e0760bc605e1de95eedcb3e6a76
felix1251 <felixabacajen17@gmail.com>
2024-09-07 01:44:56 +0800

    setup action mailer and devise reset password style adjustment

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,10 +1,10 @@
 if Rails.env.production?
   ActionMailer::Base.smtp_settings = {
-    user_name: ENV.fetch('SENDGRID_USERNAME', nil),
-    password: ENV.fetch('SENDGRID_PASSWORD', nil),
+    user_name: ENV.fetch('MAILER_USERNAME', nil),
+    password: ENV.fetch('MAILER_PASSWORD', nil),
+    address: ENV.fetch('MAILER_ADDRESS', nil),
+    port: ENV.fetch('MAILER_PORT', nil),
     domain: ENV.fetch('APP_DOMAIN', nil),
-    address: "smtp.sendgrid.net",
-    port: 587,
     authentication: :plain,
     enable_starttls_auto: true
   }
