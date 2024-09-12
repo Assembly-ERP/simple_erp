b55e334d781497eebb1468d3fcfd24075e57e439
felix1251 <felixabacajen17@gmail.com>
2024-09-12 21:33:21 +0800

    cannont press cancel button on submmiting

--- app/javascript/controllers/users/form_controller.js
+++ app/javascript/controllers/users/form_controller.js
@@ -39,12 +39,14 @@ export default class extends Controller {
     this.spinnerTarget.classList.remove("hidden");
     this.submitTarget.disabled = true;
     this.resetTarget.disabled = true;
+    this.cancelTarget.disabled = true;
   }
 
   submitEnd(event) {
     this.spinnerTarget.classList.add("hidden");
     this.submitTarget.disabled = false;
     this.resetTarget.disabled = false;
+    this.cancelTarget.disabled = false;
 
     if (event.detail.success) {
       this.cancelTarget.click();
@@ -61,7 +63,7 @@ export default class extends Controller {
     this.roleTargets[0].checked = true;
     this.customerDisplayTarget.classList.add("hidden");
     this.selectTarget.required = false;
-    this.errorTarget.innerHTML = "";
+    this.errorTarget.classList.add("hidden");
   }
 
   disconnect() {
