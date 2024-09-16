630e2f2dc8000cfd2e4c24799a204e441a2da132
felix1251 <felixabacajen17@gmail.com>
2024-09-16 23:43:51 +0800

    catalog search func

---
+++ app/javascript/controllers/catalog/search_controller.js
@@ -0,0 +1,32 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["input", "url"];
+
+  search() {
+    clearTimeout(this.timeout);
+
+    this.timeout = setTimeout(() => {
+      let path = this.element.dataset.url;
+      const searchValue = this.inputTarget.value;
+
+      if (path.includes("?")) path += `&search=${searchValue}`;
+      else path += `?search=${searchValue}`;
+
+      path += `&search_by[]=name`;
+      path += `&search_by[]=description`;
+
+      fetch(path, {
+        method: "GET",
+        headers: {
+          Accept: "text/vnd.turbo-stream.html",
+          "X-CSRF-Token": document
+            .querySelector('meta[name="csrf-token"]')
+            .getAttribute("content"),
+        },
+      })
+        .then((res) => res.text())
+        .then((html) => Turbo.renderStreamMessage(html));
+    }, 400);
+  }
+}
