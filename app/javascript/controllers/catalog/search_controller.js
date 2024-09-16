bf2caf01fdfe4b4a78574dc633f00325145eeb10
felix1251 <felixabacajen17@gmail.com>
2024-09-16 23:47:32 +0800

    catalog search func

--- app/javascript/controllers/catalog/search_controller.js
+++ app/javascript/controllers/catalog/search_controller.js
@@ -7,26 +7,30 @@ export default class extends Controller {
     clearTimeout(this.timeout);
 
     this.timeout = setTimeout(() => {
-      let path = this.element.dataset.url;
-      const searchValue = this.inputTarget.value;
-
-      if (path.includes("?")) path += `&search=${searchValue}`;
-      else path += `?search=${searchValue}`;
-
-      path += `&search_by[]=name`;
-      path += `&search_by[]=description`;
-
-      fetch(path, {
-        method: "GET",
-        headers: {
-          Accept: "text/vnd.turbo-stream.html",
-          "X-CSRF-Token": document
-            .querySelector('meta[name="csrf-token"]')
-            .getAttribute("content"),
-        },
-      })
-        .then((res) => res.text())
-        .then((html) => Turbo.renderStreamMessage(html));
+      this.searchExec();
     }, 400);
+  }
+
+  searchExec() {
+    let path = this.element.dataset.url;
+    const searchValue = this.inputTarget.value;
+
+    if (path.includes("?")) path += `&search=${searchValue}`;
+    else path += `?search=${searchValue}`;
+
+    path += `&search_by[]=name`;
+    path += `&search_by[]=description`;
+
+    fetch(path, {
+      method: "GET",
+      headers: {
+        Accept: "text/vnd.turbo-stream.html",
+        "X-CSRF-Token": document
+          .querySelector('meta[name="csrf-token"]')
+          .getAttribute("content"),
+      },
+    })
+      .then((res) => res.text())
+      .then((html) => Turbo.renderStreamMessage(html));
   }
 }
