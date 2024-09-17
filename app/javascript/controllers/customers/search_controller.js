d511a537423f07256c856c765e5531930b416cec
felix1251 <felixabacajen17@gmail.com>
2024-09-17 23:19:14 +0800

    implement search in customers

---
+++ app/javascript/controllers/customers/search_controller.js
@@ -0,0 +1,38 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["input"];
+
+  search() {
+    clearTimeout(this.timeout);
+
+    this.timeout = setTimeout(() => {
+      this.searchExec();
+    }, 400);
+  }
+
+  searchExec() {
+    let path = this.element.dataset.url;
+    const searchValue = this.inputTarget.value;
+
+    if (path.includes("?")) path += `&search=${searchValue}`;
+    else path += `?search=${searchValue}`;
+
+    path += `&search_by[]=name`;
+    path += `&search_by[]=phone`;
+    path += `&search_by[]=city`;
+    path += `&search_by[]=state`;
+
+    fetch(path, {
+      method: "GET",
+      headers: {
+        Accept: "text/vnd.turbo-stream.html",
+        "X-CSRF-Token": document
+          .querySelector('meta[name="csrf-token"]')
+          .getAttribute("content"),
+      },
+    })
+      .then((res) => res.text())
+      .then((html) => Turbo.renderStreamMessage(html));
+  }
+}
