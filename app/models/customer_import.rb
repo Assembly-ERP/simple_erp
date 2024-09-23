c471bd0d97dfc725a1089fed63ad561f5bd18445
felix1251 <felixabacajen17@gmail.com>
2024-09-23 13:27:56 +0800

    customer import upload ui

--- app/models/customer_import.rb
+++ app/models/customer_import.rb
@@ -5,6 +5,9 @@ class CustomerImport < ApplicationRecord
   ALLOWED_FILE_TYPES = %w[text/csv].freeze
   STATUSES = %w[pending success failed].freeze
 
+  # Relations
+  belongs_to :created_by, class_name: 'User'
+
   # Attachments
   has_one_attached :sheet
   has_one_attached :log
@@ -13,10 +16,29 @@ class CustomerImport < ApplicationRecord
   validates :sheet, attached: true, content_type: ALLOWED_FILE_TYPES
 
   # Generators
-  after_save :process_sheet_import
+  after_save :process_sheet_import, :previously_new_record?
 
   private
 
   # Run background job
   def process_sheet_import; end
 end
+
+# == Schema Information
+#
+# Table name: customer_imports
+#
+#  id            :bigint           not null, primary key
+#  status        :string           default("pending"), not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  created_by_id :bigint
+#
+# Indexes
+#
+#  index_customer_imports_on_created_by_id  (created_by_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (created_by_id => users.id)
+#
