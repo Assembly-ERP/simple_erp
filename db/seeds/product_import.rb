dfed8fd5ad1d5938b324e8acee7a933b2d7ca1cc
felix1251 <felixabacajen17@gmail.com>
2024-09-24 00:45:52 +0800

    can search part on order

--- db/seeds/product_import.rb
+++ db/seeds/product_import.rb
@@ -1,25 +1,25 @@
 # frozen_string_literal: true
 
-if ENV['USER_IMPORT_FILE'].present?
-  require 'csv'
-
-  path = Rails.root.join("imports/#{ENV['USER_IMPORT_FILE']}")
-
-  downcase_converter = ->(header) { header.downcase }
-  table = CSV.parse(File.read(path), headers: true, header_converters: downcase_converter)
-
-  table.each do |row|
-    product = Part.build(
-      name: row['sku'], nmfc: row['nmfc'], description: row['new description'],
-      width: row['width'], weight: row['weight']&.chomp('lbs').to_f || 0,
-      length: row['length'], sku: row['sku']
-    )
-
-    if product.valid?
-      product.save!
-      puts "Product saved: #{product.name}"
-    else
-      puts "Invalid product: #{product.errors.full_messages.join(', ')}"
-    end
-  end
-end
+# if ENV['USER_IMPORT_FILE'].present?
+#   require 'csv'
+#
+#   path = Rails.root.join("imports/#{ENV['USER_IMPORT_FILE']}")
+#
+#   downcase_converter = ->(header) { header.downcase }
+#   table = CSV.parse(File.read(path), headers: true, header_converters: downcase_converter)
+#
+#   table.each do |row|
+#     product = Part.build(
+#       name: row['sku'], nmfc: row['nmfc'], description: row['new description'],
+#       width: row['width'], weight: row['weight']&.chomp('lbs').to_f || 0,
+#       length: row['length'], sku: row['sku']
+#     )
+#
+#     if product.valid?
+#       product.save!
+#       puts "Product saved: #{product.name}"
+#     else
+#       puts "Invalid product: #{product.errors.full_messages.join(', ')}"
+#     end
+#   end
+# end
