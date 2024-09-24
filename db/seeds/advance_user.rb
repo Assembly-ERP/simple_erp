71cd8891a494f1c5682e6d70e1b1d515e0ebff5b
felix1251 <felixabacajen17@gmail.com>
2024-09-25 03:30:48 +0800

    init user env

--- db/seeds/advance_user.rb
+++ db/seeds/advance_user.rb
@@ -1,15 +1,37 @@
 # frozen_string_literal: true
 
-if ENV.fetch('SUPER_USER_EMAIL', 'admin@example.com').present? &&
-   ENV.fetch('SUPER_USER_PASSWORD', '').present?
-
+if ENV.fetch('SUPER_USER_EMAIL', '').present? && ENV.fetch('SUPER_USER_PASSWORD', '').present?
   User.create(
-    email: ENV.fetch('SUPER_USER_EMAIL', 'admin@example.com'),
-    password: ENV.fetch('SUPER_USER_PASSWORD', 'password'),
-    password_confirmation: ENV.fetch('SUPER_USER_PASSWORD', 'password'),
+    email: ENV.fetch('SUPER_USER_EMAIL', ''),
+    password: ENV.fetch('SUPER_USER_PASSWORD', ''),
+    password_confirmation: ENV.fetch('SUPER_USER_PASSWORD', ''),
     role: 'super_user',
     first_name: 'Super',
     last_name: 'User',
+    confirmed_at: Time.now.utc
+  )
+end
+
+if ENV.fetch('INIT_ADMIN_EMAIL', '').present? && ENV.fetch('INIT_ADMIN_PASSWORD', '').present?
+  User.create(
+    email: ENV.fetch('INIT_ADMIN_EMAIL', ''),
+    password: ENV.fetch('INIT_ADMIN_PASSWORD', ''),
+    password_confirmation: ENV.fetch('INIT_ADMIN_PASSWORD', ''),
+    role: 'super_user',
+    first_name: ENV.fetch('INIT_ADMIN_FIRST_NAME', 'Admin'),
+    last_name: ENV.fetch('INIT_ADMIN_LAST_NAME', 'User'),
+    confirmed_at: Time.now.utc
+  )
+end
+
+if ENV.fetch('INIT_MANAGER_EMAIL', '').present? && ENV.fetch('INIT_MANAGER_PASSWORD', '').present?
+  User.create(
+    email: ENV.fetch('INIT_MANAGER_EMAIL', ''),
+    password: ENV.fetch('INIT_MANAGER_PASSWORD', ''),
+    password_confirmation: ENV.fetch('INIT_MANAGER_PASSWORD', ''),
+    role: 'super_user',
+    first_name: ENV.fetch('INIT_MANAGER_FIRST_NAME', 'Manager'),
+    last_name: ENV.fetch('INIT_MANAGER_LAST_NAME', 'User'),
     confirmed_at: Time.now.utc
   )
 end
