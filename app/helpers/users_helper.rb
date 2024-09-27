30956099ecafbb0b25c277e66d6fa0493d89a5c4
felix1251 <felixabacajen17@gmail.com>
2024-09-28 04:04:37 +0800

    cleanup helpers

--- app/helpers/users_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module UsersHelper
-  def format_phone_number(phone)
-    return '' if phone.nil?
-
-    phone.gsub(/\D/, '').gsub(/(\d{3})(\d{3})(\d{4})/, '\1-\2-\3')
-  end
-end
