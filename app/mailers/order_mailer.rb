02cad86a6855877e61e2a97a608b18aecc423e4b
felix1251 <felixabacajen17@gmail.com>
2024-09-27 18:42:43 +0800

    action mailer wwith sidekiq

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -8,6 +8,10 @@ class OrderMailer < ApplicationMailer
     @order = order
     @is_quote = !@order.order_status.customer_locked
 
+    quote_or_invoice = OrderPdf.new(@order)
+    filename = "order_#{@is_quote ? 'quote' : 'invoice'}_#{@order.formatted_id}_#{Time.zone.now.to_i}.pdf"
+    attachments[filename] = { mime_type: 'application/pdf', content: quote_or_invoice.render }
+
     @order.users.each do |user|
       mail(to: user.email, subject: "#{@is_quote ? 'Quote' : 'Invoice'}: Order #{@order.id}")
     end
