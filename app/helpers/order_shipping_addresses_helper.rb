1da180790af521f0693c3e7dcd8c889b7e3e47c2
felix1251 <felixabacajen17@gmail.com>
2024-09-29 23:30:13 +0800

    updare order address helper update

--- app/helpers/order_shipping_addresses_helper.rb
+++ app/helpers/order_shipping_addresses_helper.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 module OrderShippingAddressesHelper
-  def order_readable_shipping_address(shipping_address:)
-    return '' if shipping_address.blank?
+  def order_readable_shipping_address(order)
+    shipping_address = order.order_shipping_address
 
-    "#{shipping_address.street ? "#{shipping_address.street}," : ''} " \
-      "#{shipping_address.city ? "#{shipping_address.city}," : ''}  " \
+    return nil if shipping_address.blank?
+
+    "#{shipping_address.street.present? ? "#{shipping_address.street}, " : ' '}" \
+      "#{shipping_address.city.present? ? "#{shipping_address.city}, " : ' '}" \
       "#{shipping_address.state} #{shipping_address.zip_code}"
   end
 end
