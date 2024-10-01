b60275ba74c81f6a5fb4d9ca8236594198eae49f
felix1251 <felixabacajen17@gmail.com>
2024-10-02 00:11:14 +0800

    update api controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,17 @@
 module Api
   module V1
     class UsersController < BaseController
+      authorize_resource class: false, only: :me
+
       def sign_in; end
+
+      def me; end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:email, :password)
+      end
     end
   end
 end
