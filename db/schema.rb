972335a2fddd668f5a81ad6052b015afde4949b3
felix1251 <felixabacajen17@gmail.com>
2024-10-05 14:43:57 +0800

    ordetails must be unique by order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_27_092514) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_05_063252) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -125,6 +125,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_27_092514) do
     t.boolean "override", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["order_id", "part_id", "product_id"], name: "index_order_details_on_order_id_and_part_id_and_product_id", unique: true
     t.index ["order_id"], name: "index_order_details_on_order_id"
     t.index ["part_id"], name: "index_order_details_on_part_id"
     t.index ["product_id"], name: "index_order_details_on_product_id"
