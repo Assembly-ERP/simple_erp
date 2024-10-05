5be2fbf875cc824607d0156bfdb18581405f1f73
felix1251 <felixabacajen17@gmail.com>
2024-10-05 19:02:41 +0800

    api v1 users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -32,7 +32,8 @@ module Api
       private
 
       def user_params
-        params.require(:user).permit(:first_name, :last_name, :email, :role, :phone, :password, :password_confirmation)
+        params.require(:user)
+              .permit(:first_name, :last_name, :email, :role, :phone, :password, :password_confirmation, :advance)
       end
     end
   end
