0e763c7aabd44a8c1c0d719fc5960e18a34d956b
felix1251 <felixabacajen17@gmail.com>
2024-12-05 11:32:45 +0800

    integration piece

--- lib/integration/billing.rb
+++ lib/integration/billing.rb
@@ -2,5 +2,13 @@
 
 module Integration
   class Billing
+    def initialize
+      @conn = Faraday.new(
+        url: 'https://example.com',
+        headers: { 'Content-Type' => 'application/json' }
+      )
+    end
+
+    def pay_order; end
   end
 end
--- lib/integration/logistic.rb
+++ lib/integration/logistic.rb
@@ -2,5 +2,13 @@
 
 module Integration
   class Logistic
+    def initialize
+      @conn = Faraday.new(
+        url: 'https://example.com',
+        headers: { 'Content-Type' => 'application/json' }
+      )
+    end
+
+    def ship_order; end
   end
 end
