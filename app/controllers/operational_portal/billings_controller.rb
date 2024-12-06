336aee56f59ae495d32822c5698a43c18fe6ad64
felix1251 <felixabacajen17@gmail.com>
2024-12-06 12:48:17 +0800

    billing ui

--- app/controllers/operational_portal/billings_controller.rb
+++ app/controllers/operational_portal/billings_controller.rb
@@ -2,7 +2,7 @@
 
 module OperationalPortal
   class BillingsController < OperationalPortal::ManageBaseController
-    authorize_resource class: false
+    load_and_authorize_resource
 
     before_action :set_billing
 
